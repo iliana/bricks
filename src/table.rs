588259c25f2f97cd930831bcc01a7f54b7a9bf59
iliana etaoin <iliana@buttslol.net>
2021-12-05 13:04:29 -0800

    table insertion

--- src/table.rs
+++ src/table.rs
@@ -63,6 +63,24 @@ where
         }
     }
 
+    pub fn insert<const M: usize, const Z: usize>(self, index: usize, other: Table<M>) -> Table<Z>
+    where
+        [String; Z]: Default,
+        [&'static str; Z]: Default,
+    {
+        Table {
+            header: array_insert(self.header, other.header, index),
+            abbr: array_insert(self.abbr, other.abbr, index),
+            col_class: array_insert(self.col_class, other.col_class, index),
+            rows: self
+                .rows
+                .into_iter()
+                .zip(other.rows)
+                .map(|(a, b)| a.insert(index, b))
+                .collect(),
+        }
+    }
+
     pub fn with_totals<const S: usize>(self, totals: [impl ToString; S]) -> TotalsTable<N, S>
     where
         [String; S]: Default,
@@ -83,6 +101,22 @@ pub struct Row<const N: usize> {
     pub data: [String; N],
     pub href: [String; N],
     pub class: &'static str,
+}
+
+impl<const N: usize> Row<N>
+where
+    [String; N]: Default,
+{
+    fn insert<const M: usize, const Z: usize>(self, index: usize, other: Row<M>) -> Row<Z>
+    where
+        [String; Z]: Default,
+    {
+        Row {
+            data: array_insert(self.data, other.data, index),
+            href: array_insert(self.href, other.href, index),
+            class: self.class,
+        }
+    }
 }
 
 impl<const N: usize> Default for Row<N>
@@ -110,4 +144,57 @@ impl<const N: usize, const S: usize> Deref for TotalsTable<N, S> {
     fn deref(&self) -> &Table<N> {
         &self.table
     }
+}
+
+/// Creates a new array with the elements of `a`, with the elements of `b` inserted at `index` of
+/// `a`.
+fn array_insert<T, const N: usize, const M: usize, const Z: usize>(
+    a: [T; N],
+    b: [T; M],
+    index: usize,
+) -> [T; Z]
+where
+    [T; Z]: Default,
+{
+    assert_eq!(N + M, Z);
+    assert!(index <= a.len());
+
+    let mut new: [T; Z] = Default::default();
+    let mut a = a.into_iter();
+    let mut i = 0;
+
+    while i < index {
+        new[i] = a.next().unwrap();
+        i += 1;
+    }
+    for x in b {
+        new[i] = x;
+        i += 1;
+    }
+    for x in a {
+        new[i] = x;
+        i += 1;
+    }
+
+    new
+}
+
+#[cfg(test)]
+#[test]
+fn test_array_insert() {
+    assert_eq!(array_insert([1, 2, 3], [], 2), [1, 2, 3]);
+
+    assert_eq!(array_insert([1, 2, 3], [4], 0), [4, 1, 2, 3]);
+    assert_eq!(array_insert([1, 2, 3], [4], 1), [1, 4, 2, 3]);
+    assert_eq!(array_insert([1, 2, 3], [4], 2), [1, 2, 4, 3]);
+    assert_eq!(array_insert([1, 2, 3], [4], 3), [1, 2, 3, 4]);
+
+    assert_eq!(array_insert([1, 2], [3, 4, 5], 1), [1, 3, 4, 5, 2]);
+}
+
+#[cfg(test)]
+#[test]
+#[should_panic]
+fn test_array_insert_panic() {
+    assert_eq!(array_insert([1, 2, 3], [4], 2), [1, 2, 4, 3, 5]);
 }
