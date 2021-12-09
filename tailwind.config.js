a02de21092444104c24ba321a36b43a31d0e6efb
iliana etaoin <iliana@buttslol.net>
2021-12-08 17:35:29 -0800

    2xl containers, but xl for box scores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@ module.exports = {
   purge: ["./templates/*.html", "./templates/*.js", "./src/**/*.rs", "./node_modules/tablesort/dist/**.js"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-    },
     extend: {},
   },
   variants: {
